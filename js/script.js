8e9154aa2c042d00d27bbc07a5f088e49252ba3d
Nick-Bueltel <nickbueltel@gmail.com>
2019-05-04 10:38:44 -0500

    added sounds

--- js/script.js
+++ js/script.js
@@ -4,11 +4,26 @@ var inputBox = document.getElementById("inputBox");
 var boxButton = document.getElementById("boxButton");
 var onlyButton = document.getElementById("onlyButton");
 var title = document.getElementById("title");
+var points = document.getElementById("pointsDiv");
+
 
 
 //define audio constants to call inside of the objects.
 var hahah = new Audio('SE/ahaha.wav');
 var cicadas = new Audio("SE/cicadas.ogg");
+var ds1 = new Audio("SE/deathsounds/died1.ogg");
+var ds2 = new Audio("SE/deathsounds/died2.ogg");
+var ds3 = new Audio("SE/deathsounds/died3.ogg");
+var ds4 = new Audio("SE/deathsounds/died4.ogg");
+var apathy = new Audio("BGM/apathy.ogg");
+var rChanger = new Audio("BGM/rhythm-changer.ogg")
+
+
+//audio loop definitions 
+cicadas.loop=true;
+apathy.loop=true;
+rChanger.loop=true; 
+
 
 //puzzles - please dont spoil it for yourself. 
 class Puzzle{
@@ -34,12 +49,13 @@ class Puzzle{
 
 //define puzzle objects from class 
 const puzzle1 = new Puzzle('https://i.imgur.com/j0UMwfZ.jpg', '19140728', cicadas);
-const puzzle2 = new Puzzle('https://i.imgur.com/NqkqByd.jpg', '682', cicadas);
-const puzzle3 = new Puzzle('https://i.imgur.com/3PBJQOy.jpg', '137', cicadas);
+const puzzle2 = new Puzzle('https://i.imgur.com/NqkqByd.jpg', '682', rChanger);
+const puzzle3 = new Puzzle('https://i.imgur.com/3PBJQOy.jpg', '137', apathy);
 const puzzle4 = new Puzzle('#', '#');
 
 //puzzle array
 const pzArray = [puzzle1, puzzle2, puzzle3];
+const dSounds = [ds1, ds2, ds3, ds4];
 
 //classless functions 
 function startPuzzle(){
@@ -47,11 +63,24 @@ function startPuzzle(){
     thispuzzle = pzArray[Math.floor(Math.random()*pzArray.length)]; 
     while (thispuzzle.played === true){
         thispuzzle = pzArray[Math.floor(Math.random()*pzArray.length)];
+
     }; 
     
+    
     playingGame();
+    setPoints();
 
-};
+}
+
+function setPoints(){
+    pointsTotal = 50;
+    points.innerHTML = pointsTotal;  
+}
+
+function deductPoints(){
+    pointsTotal = pointsTotal -5; 
+    points.innerHTML = pointsTotal; 
+}
 
 function playingGame(){
     image.style.visibility = 'visible'
@@ -71,6 +100,12 @@ function resetGame(){
     title.style.visibility = 'visible';
 }
 
+function punish(){
+    image.setAttribute('src', 'https://i.imgur.com/cBYUTlr.jpg');
+    thispuzzle.bgm.pause();
+    //sound.play();
+}
+
 
 //onclick function for main button. determines if the src attribute of imageA has been assigned yet. if not then it will invoke startPuzzle()
 function determineEligibility(){
@@ -78,17 +113,8 @@ function determineEligibility(){
         startPuzzle();
     } else {
         return; 
-    }
-    
+    }   
 }
-//function to count the amount of wrong answers input into the box
-function countWrong(){     
-    var i;
-    i++;
-        console.log(i);
-    }
-
-
 //checkAnswer function to determine if the correct pass has been entered
 function checkAnswer(){
     answer = document.getElementById("inputBox");
@@ -97,10 +123,17 @@ function checkAnswer(){
         thispuzzle.played = true; 
         //refactor below code into a function 
         resetGame();
+        
 
+    } else if(pointsTotal === 0){
+        punish();
+        var dSound = dSounds[Math.floor(Math.random() * dSounds.length)];
+        dSound.play();
+        
+    
     } else {
         hahah.play();
-        countWrong();
+        deductPoints();
         return; 
     }
 
